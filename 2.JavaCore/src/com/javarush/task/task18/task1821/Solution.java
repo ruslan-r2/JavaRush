d01925c364e47b86755af886fb9f55ecaef26595
ruslan-r2 <ruslan_r@digann.ru>
2019-12-17 20:13:06 +0300

    JavaRush task 1821

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -0,0 +1,28 @@
+package com.javarush.task.task18.task1821;
+/*
+Встречаемость символов
+*/
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        String[] arg = {"file.txt"};
+        FileInputStream inputStream = new FileInputStream( arg[0] );
+
+        int[] arrByte = new int[Byte.MAX_VALUE*2];
+        while ( inputStream.available() > 0 ){
+            int b = inputStream.read();
+                arrByte[b]++;
+        }
+        inputStream.close();
+
+        for (int i = 0; i < arrByte.length; i++) {
+            if ( arrByte[i] > 0 ) {
+                char ch = (char) i;
+                System.out.println( ch + " " + arrByte[i]);
+            }
+        }
+
+    }
+}
