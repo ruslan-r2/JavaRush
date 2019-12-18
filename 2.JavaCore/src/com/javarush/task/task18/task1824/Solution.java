c9202490ae523e1266ce7b70a06b2beb10dd5ed0
ruslan-r2 <ruslan_r@digann.ru>
2019-12-18 19:16:06 +0300

    JavaRush task 1815

---
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task18.task1824;
+/*
+Файлы и исключения
+*/
+import java.io.*;
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while ( true ) {
+            String fileName = reader.readLine();
+            try {
+                FileInputStream inputStream = new FileInputStream(fileName);
+                inputStream.close();
+            } catch (FileNotFoundException e) {
+                reader.close();
+                System.out.println(fileName);
+                return;
+            }
+        }
+    }
+}
