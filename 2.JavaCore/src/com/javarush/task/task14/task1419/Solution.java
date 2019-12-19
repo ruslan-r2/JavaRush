1b8cb62c3e377538e9fe448dad60d60fc8cfabc9
ruslan-r2 <ruslan_r@digann.ru>
2019-12-19 20:33:08 +0300

    JavaRush task 1419

--- 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1419/Solution.java
@@ -1,23 +1,15 @@
 package com.javarush.task.task14.task1419;
 
-import com.sun.corba.se.impl.presentation.rmi.ExceptionHandler;
-import com.sun.org.apache.bcel.internal.ExceptionConstants;
-
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.text.*;
 import java.util.*;
-
-/* 
+/*
 Нашествие исключений
 */
-
 public class Solution {
     public static List<Exception> exceptions = new ArrayList<Exception>();
-
     public static void main(String[] args) {
         initExceptions();
-
         for (Exception exception : exceptions) {
             System.out.println(exception);
         }
@@ -26,32 +18,27 @@ public class Solution {
     private static void initExceptions() {   //the first exception
         try {
             float i = 1 / 0;
-
         } catch (Exception e) {
             exceptions.add(e);
         }
-
         //напишите тут ваш код
         try {
             Integer a = Integer.parseInt("строка");
         } catch (Exception e2) {
             exceptions.add(e2);
         }
-
         try {
             Integer b = null;
             System.out.println(b / 2);
         } catch (Exception e3) {
             exceptions.add(e3);
         }
-
         try {
             int[] c = {1,2,3};
             System.out.println(c[4]);
         } catch (Exception e4){
             exceptions.add(e4);
         }
-
         try {
             Map<String, String> map = new HashMap<>();
             map.put("1", "Один");
@@ -66,40 +53,35 @@ public class Solution {
         } catch (Exception e5) {
             exceptions.add(e5);
         }
-
         try {
             FileInputStream fis = new FileInputStream("C:\\Users\\17302957\\text.txt");
             fis.close();
         } catch (Exception e6) {
             exceptions.add(e6);
         }
-
         try {
-            Integer Value = Integer.parseInt(null);
+            String date = "2013-10-22 17:45:53";
+            DateFormat dformat = new SimpleDateFormat("MMMMM d yyyy");
+            dformat.parse(date);
         }catch (Exception e7){
             exceptions.add(e7);
         }
-
         try {
             int[] arrInt = new int[-1];
         } catch (Exception e8) {
             exceptions.add(e8);
         }
-
         try {
             Object x = new Integer(0);
             System.out.println((String)x);
         } catch (Exception e9) {
             exceptions.add(e9);
         }
-
         try {
             List<Integer> fixedSize = Arrays.asList(1,2,3);
             fixedSize.add(4);
-
         } catch (Exception e10) {
             exceptions.add(e10);
         }
-
     }
 }
