6b8a6d444218c6d4e8bda0e9ee460604914c75dd
ruslan-r2 <ruslan_r@digann.ru>
2019-12-25 20:54:07 +0300

    JavaRush task 1913

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1913/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task19.task1913;
+
+/* 
+Выводим только цифры
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream console = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream myStream = new PrintStream( outputStream );
+
+        System.setOut( myStream );
+        testString.printSomething();
+
+        String str = outputStream.toString().replaceAll("[^0-9]","");
+
+        System.setOut( console );
+        System.out.print( str );
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
